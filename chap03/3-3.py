f1f7929ce0a4fa6bb8e27c4f4af9281a33bb87cb
YJ <jyjin1206@gmail.com>
2024-03-23 16:47:14 +0900

    Complete chapter3

---
+++ chap03/3-3.py
@@ -0,0 +1,14 @@
+# 오츄 알고리즘으로 이진화하기
+import cv2 as cv
+import sys
+
+img = cv.imread("soccer.jpg")
+
+t, bin_img = cv.threshold(img[:,:,2], 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+print("오츄 알고리즘이 찾은 최적 임계값=t",)
+
+cv.imshow("R channel", img[:,:,2]) # R 채널 영싱
+cv.imshow("T channel binarization", bin_img) # R 채널 이진화 영상
+
+cv.waitKey()
+cv.destroyAllWindows()
